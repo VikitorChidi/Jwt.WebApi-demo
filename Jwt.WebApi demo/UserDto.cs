f383b1ae4560f669b933b46bdac80b9480f47aca
Victor Chidi <victorblessed9@gmail.com>
2022-01-30 01:51:22 +0100

    Add project files.

---
+++ Jwt.WebApi demo/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Jwt.WebApi_demo
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
